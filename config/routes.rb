e2e22938439cc0714b908cc6aa8118710fbc20e3
zuren <richard@rpf.me>
2015-04-07 00:39:09 +0100

    oauth integrated

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
     root 'home#index'
 
+    # OAuth paths
+    get '/auth/:provider/callback', to: 'sessions#create'
+    get '/logout', to: 'sessions#destroy'
+
 end
